c963dd072f7860d24a3b2a5d2e68aebe9151b3b2
Anna Sparapani <113983183+annasparapani@users.noreply.github.com>
2023-04-17 12:24:28 +0200

    Added pH reading to the incubator code
    ***** QUESTION ******
    the code seems to be doing the same thing - reading sensors and printing them on LCD twice. Is this necessary?

--- codiceincubatoriLTMP/codiceincubatoriLTMP.ino
+++ codiceincubatoriLTMP/codiceincubatoriLTMP.ino
@@ -33,17 +33,27 @@ Pin [ROWS] # 33, 35, 37,39 : connect to the row pinouts of the keypad
 Pin [COLS] # 41, 43, 45 : connect to the column pinouts of the keypad
 rowPins_m[4] # 33, 35, 37,39 : connect to the row pinouts of the keypad
 colPins_m[1] # 47: connect to the column pinouts of the keypad
+---- WE ADD 
+
+pin #3: Rx from pH 
+pin #4: Tx from pH 
+
 */
 
 // Librerie 
 
 #include <cozir.h> // management of CO2 sensor, which is a COZIR sensor 
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h> // serial communication with LCD screen and pH sensor EVO board
  
-
 #define ONE_WIRE_BUS 2
+// CO2 sensor 
 SoftwareSerial sws(12, 13);
 COZIR czr(&sws);
+
+// pH sensor 
+#define rx_pH 3
+#define tx_pH 4
+SoftwareSerial myserial(rx_pH, tx_pH); 
 
 
 #include <LiquidCrystal_I2C.h> // manages communication with LCD crystal that uses I2C protocol (has lcd variable type already defined inside)
@@ -108,11 +118,18 @@ int i; //USO VARIALE INTERA COME CONTEGGIO
 int soglia_n=0;
 int CO2Threshold_n=0;
 float dueore=2*60*60000; //limite di due ore da collegare al conteggio della funzione millis()
-
+String intput_string_pH = "";
+String sensor_string_pH = "";
+boolean input_string_pH_complete=false; 
+booleas sensor_string_pH_complete=false; 
+float pH; 
 
 void setup() {
   Serial.begin(9600); // baudrate a 9600
-  sws.begin(9600);
+  myserial.begin(9600); // pH sensor begins with baudrate 9600
+  sws.begin(9600); // CO2 sensor begins with baudrate 9600 
+  inputstring.reserve(10);     //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);     //set aside some bytes for receiving data from Atlas Scientific product
   czr.init();
   lcd.init();
   lcd.backlight();
@@ -141,11 +158,21 @@ void setup() {
 
 }
 
+void serialEvent_pH(){
+  inputstring_pH = Serial.readStringUntil(13); 
+  input_string_pH_complete=true; 
+}
+
 void loop() {
 
   tempo_riempimento = millis(); //inizio il conteggio del tempo , returns the number of milliseconds since the arduino board was powered or reset, 32-bit unsigned int (enough for our times)
- 
-     float c = czr.CO2()*multiplier;
+  
+  /* QUESTION 
+  it's doing the reading twise, once in this loop function, and 
+  another time in the LCD function - is it necessary? can't we just pass the parameters
+  to the LCD function? */ 
+
+    float c = czr.CO2()*multiplier;
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("CO2:");
@@ -156,69 +183,102 @@ void loop() {
           lcd.print(soglia);
           lcd.print("%"); 
                   
-//float t=sensors.getTempCByIndex(0);
- float t = dht.readTemperature(); 
-          lcd.setCursor(0, 1);
-          lcd.print("T:"); 
-          lcd.setCursor(2, 1);  
-          lcd.print(t);
-          lcd.print(" C");
+    //float t=sensors.getTempCByIndex(0);
+    float t = dht.readTemperature(); 
+              lcd.setCursor(0, 1);
+              lcd.print("T:"); 
+              lcd.setCursor(2, 1);  
+              lcd.print(t);
+              lcd.print(" C");
 
- int h = dht.readHumidity();
-          lcd.setCursor(10, 1);
-          lcd.print("U:"); 
-          lcd.setCursor(12, 1);  
-          lcd.print(h);
-          lcd.print("%");
+    int h = dht.readHumidity();
+              lcd.setCursor(10, 1);
+              lcd.print("U:"); 
+              lcd.setCursor(12, 1);  
+              lcd.print(h);
+              lcd.print("%");
 
-        digitalWrite(pin_r,LOW);
-        digitalWrite(pin_v,LOW);
-        digitalWrite(solenoide, LOW);
-        
-         
-         if(c!=0)
-          {
+            digitalWrite(pin_r,LOW);
+            digitalWrite(pin_v,LOW);
+            digitalWrite(solenoide, LOW);
+  
+  /*****************************************
+  *********** EZO pH board reading**********
+  *******************************************/ 
+  // **** pH value reading ****
+  if (myserial.available() > 0) {                //if we see that the Atlas Scientific product has sent a character
+      char inchar = (char)myserial.read();       //get the char we just received
+      sensorstring += inchar;                    //add the char to the var called sensorstring
+      if (inchar == '\r') {                      //if the incoming character is a <CR>
+      sensor_string_complete = true;             //set the flag
+      }
+  }
 
-          if (c < soglia && c < CO2Threshold*soglia) {   //*********  Riempimento iniziale - misuro la CO2  ***************//
-            
-           digitalWrite(pin_r,HIGH);                    
-           digitalWrite(solenoide,HIGH);//aperto           
-           digitalWrite(pin_v,LOW);  
-           // digitalWrite(solenoide, LOW);                //*********   Apertura solo per 'SolenoidOnTime'   ***************//
-           //delay(SolenoidOnTime);
-           // digitalWrite(solenoide, HIGH);
-           //digitalWrite(pin_r,HIGH);
-          } 
-          if (c < soglia && c >= CO2Threshold*soglia) {  //*********************    Soglia minore   ***********************//
-           
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+      Serial.println(sensorstring);                   //send that string to the PC's serial monitor
+      if (isdigit(sensorstring[0])) {                 //if the first character in the string is a digit, we convert it to a float 
+      pH = sensorstring.toFloat(); 
+      lcd.setCursor(0, 2);
+      lcd.print("pH:"); 
+      lcd.setCursor(5, 2);  
+      lcd.print(pH);
+    }
+  }
+  // **** reading of some other information coming from the EXO board ****
+  if(input_string_pH_complete=true){
+      myserial.print(input_string_pH); // print the pH value on the serial terminal (our computer)
+      myserial.print('\r'); 
+      input_string_pH=""; 
+      input_string_pH_complete=false;
+    }
+
+    if (myserial.available() > 0) {           //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();      //get the char we just received
+    sensorstring_pH += inchar;                //add the char to the var called sensorstring
+    if (inchar == '\r') {                     //if the incoming character is a <CR>
+      sensor_string_complete_pH = true;       //set the flag
+    }
+  }
+
+    
+    if(c!=0){
+      if (c < soglia && c < CO2Threshold*soglia) {   //*********  Riempimento iniziale - misuro la CO2  ***************//
+          digitalWrite(pin_r,HIGH);                    
+          digitalWrite(solenoide,HIGH);//aperto           
+          digitalWrite(pin_v,LOW);  
+          // digitalWrite(solenoide, LOW);                //*********   Apertura solo per 'SolenoidOnTime'   ***************//
+          //delay(SolenoidOnTime);
+          // digitalWrite(solenoide, HIGH);
+          //digitalWrite(pin_r,HIGH);
+      } 
+
+      if (c < soglia && c >= CO2Threshold*soglia) {  //*********************    Soglia minore   ***********************//
             digitalWrite(solenoide, LOW);                //*********   Apertura solo per 'SolenoidOnTime'   ***************//
             delay(SolenoidOnTime2);
             digitalWrite(solenoide, HIGH);
             digitalWrite(pin_r,HIGH);
-          } 
+      } 
 
-            if (c >= soglia){                            //*********************    Soglia maggiore   *********************//
-            
+      if (c >= soglia){                            //*********************    Soglia maggiore   *********************//
              digitalWrite(pin_r,LOW); 
              digitalWrite(solenoide, LOW);//chiuso
              digitalWrite(pin_v,HIGH);
-          }
+      }
 
-          if (c!=0 && tempo_riempimento >= dueore && c <= 3.5) {
-           
+      if (c!=0 && tempo_riempimento >= dueore && c <= 3.5) {
               digitalWrite(solenoide, LOW);//chiuso
-          }
-          
-          }
-          if (c==0 && digitalRead(solenoide)==HIGH) {
-            count++;
-            Serial.print(" zeri= ");
-            Serial.println(count);
-          }
-          delay(1000);// one reading each second
-          
-          // put your main code here, to run repeatedly:  
-          updateLCD();
+      }
+    }
+    if (c==0 && digitalRead(solenoide)==HIGH) {
+        count++;
+        Serial.print(" zeri= ");
+        Serial.println(count);
+    }
+    //delay(1000);// one reading each second -> in this way reading a serial UART 
+    // becomes complicated, the delay can be moved to the update_LCD function 
+    
+    // put your main code here, to run repeatedly:  
+  updateLCD();
 }
 
 // Funzione per aggiornare il display LCD
@@ -249,6 +309,7 @@ void updateLCD() {
                     lcd.print(h);
                     lcd.print("%");
 
+
           /* Print results also on the serial monitor for debugging purposes! */
                   //*********************          CO2              *********************
               
@@ -261,5 +322,11 @@ void updateLCD() {
 
                   Serial.print(" Umid= ");
                   Serial.println(h);
+
+                  Serial.print(" pH= ");
+                  Serial.println(pH);
+
+
+    delay(1000); // moved here the delay
 }
            
