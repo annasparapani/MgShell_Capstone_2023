c199ae600cec53ca7b1ec81ab81956e830462df1
GianlucaDF <47785333+GianlucaDF@users.noreply.github.com>
2023-04-30 12:29:08 +0200

    Ora il controllo delle elettrovalvole può essere rimosso settando valves_linked = false

--- main/main.ino
+++ main/main.ino
@@ -10,7 +10,7 @@ int pwm = 10;
 int activation_time = 2000;
 int measurement_time= 3000; 
 int sleep_time = 10000;
-
+bool valves_linked = true;
 
 pump p_pump(pwm); // create a pump object  with a set pwm
 valves e_valves(); // create a valves object
@@ -23,29 +23,35 @@ void setup() {
 void loop() {
 
   // FILL  THE CHAMBER
+  if (valves_linked) {
 
-  e_valves.turnON(); // activate the valves
+    e_valves.turnON(); // activate the valves
 
-  delay(2000); //wait untill the valves are fully opened
+    delay(2000); //wait untill the valves are fully opened
+  };
 
   p_pump.turnON('HIGH'); // activate the rotation in one direction
   delay(activation_time)) // wait untill filled
   p_pump.turnOFF();
 
   delay(2000); // wait untill the pump has fully stopped
-
-  e_valves.turnOFF() // turn off the valve
+  if (valves_linked) {
+    e_valves.turnOFF();
+  };
+   // turn off the valve
 
   // DEACTIVATE WHILE MEASURING PH
-  delay(measurement_time) // wait untill the pH sensor has finished the measurement
+  delay(measurement_time);// wait untill the pH sensor has finished the measurement
 
   // EMPTY THE CHAMBER
   p_pump.turnON('LOW'); // activate the rotation in the opposite direction
-  delay(activation_time)) // wait untill emptied
+  delay(activation_time);// wait untill emptied
   p_pump.turnOFF();
+  if (valves_linked) {
+      delay(2000); // wait untill the pump has fully stopped
+      e_valves.turnOFF() // close the valves
 
-  delay(2000); // wait untill the pump has fully stopped
-  e_valves.turnOFF() // close the valves
+  };
 
   //SLEEP
   delay(sleep_time) // wait untill the next measurement phase
